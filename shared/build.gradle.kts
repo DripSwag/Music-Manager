674f85aef926e6c5e70d1a2e69ec0a2bcbeb9870
DripSwag <ethanpineda105@gmail.com>
2026-07-31 12:10:59 +0100

    Added jid3lib and metadata-extractor

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 
 kotlin {
     jvm()
-    
-    
+
+
     sourceSets {
         commonMain.dependencies {
             implementation(libs.compose.runtime)
@@ -28,6 +28,8 @@ kotlin {
             implementation("io.github.vinceglb:filekit-dialogs-compose:0.14.2")
             implementation("io.github.vinceglb:filekit-coil:0.14.2")
 
+            implementation("com.drewnoakes:metadata-extractor:2.21.0")
+            implementation("net.sf.javamusictag:jid3lib:0.5.4")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- shared/src/commonMain/kotlin/music/manager/lib/SongSourceReader.kt
+++ shared/src/commonMain/kotlin/music/manager/lib/SongSourceReader.kt
@@ -1,15 +1,29 @@
 package music.manager.lib
 
+import org.farng.mp3.MP3File
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.attribute.UserDefinedFileAttributeView
 
 class SongSourceReader {
     companion object {
         fun readAllSourceSongs(directory: File): List<String> {
             for (file: File in directory.listFiles()) {
+                handleExtension(file)
             }
             return ArrayList<String>()
         }
+    }
+}
+
+fun handleExtension(file: File) {
+    if (file.extension == "mp3") {
+        handleMP3(file)
+    }
+}
+
+fun handleMP3(file: File) {
+    val mp3File = MP3File(file)
+    if (mp3File.hasID3v2Tag()) {
+        val tag = mp3File.iD3v2Tag
+        println(tag.leadArtist)
     }
 }
